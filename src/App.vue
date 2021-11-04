3891fd0d23a7257cbd77ec3af8261072e2cb958f
nazartsap <tsap.n.i20@gmail.com>
2021-11-04 13:49:51 +0300

    2.2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <v-app>
+ <v-app>
   <v-navigation-drawer app></v-navigation-drawer> 
   <v-app-bar app dark color="primary">
     <v-app-bar-nav-icon></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text>Link One</v-btn> 
+       <v-btn  text><v-icon left>mdi-cake-variant</v-icon>Link One</v-btn> 
        <v-btn  text>Link Two</v-btn> 
        <v-btn  text>Link Three</v-btn>
     </v-toolbar-items>
   </v-app-bar>
-  <v-main>
+  <v-content>
     <router-view></router-view> 
-  </v-main>
+  </v-content>
   </v-app>
 
 </template>
