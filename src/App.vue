4d55af93f3560ced72b26db4fdcffb0cdec82503
nazartsap <tsap.n.i20@gmail.com>
2021-11-05 15:32:19 +0300

    2.3

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
- <v-app>
-  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-app>
+  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
   <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
        <v-btn  text><v-icon left>mdi-cake-variant</v-icon>Link One</v-btn> 
@@ -14,11 +14,14 @@
     <router-view></router-view> 
   </v-content>
   </v-app>
-
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      drawer: false
+    }
+  }
 }
 </script>
