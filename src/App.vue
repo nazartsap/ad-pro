48ea5113bae548c6e44915ae2754bd14fe468d0e
nazartsap <tsap.n.i20@gmail.com>
2021-11-07 21:53:43 +0300

    2.4

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,44 @@
 <template>
   <v-app>
-  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
+  <v-navigation-drawer app v-model="drawer">
+    <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            КИПУ
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Учебный проект
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item-group color="primary">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-cake-variant</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>Link One</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+  </v-navigation-drawer>  
   <v-app-bar app dark color="primary">
     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text><v-icon left>mdi-cake-variant</v-icon>Link One</v-btn> 
-       <v-btn  text>Link Two</v-btn> 
-       <v-btn  text>Link Three</v-btn>
+       <v-btn  text><v-icon left>mdi-help</v-icon>QnA</v-btn> 
+       <v-btn  text><v-icon left>mdi-login-variant</v-icon>Login in</v-btn> 
+       <v-btn  text><v-icon left>mdi-account-arrow-right-outline</v-icon>Sign up</v-btn>       
     </v-toolbar-items>
   </v-app-bar>
-  <v-content>
+  <v-main>
     <router-view></router-view> 
-  </v-content>
+  </v-main>
   </v-app>
 </template>
 
