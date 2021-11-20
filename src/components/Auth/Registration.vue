68f7cd0e5d2dc394d1f09d4640280c7097cda76e
nazartsap <tsap.n.i20@gmail.com>
2021-11-20 18:59:22 +0300

    4.2

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -3,33 +3,45 @@
 		<v-layout align-center justify-center> 
 			<v-flex xs12 sm8 md8>
 			<v-card class="elevetion-12">
-				<v-toolbar dark color="primary">Registration</v-toolbar>
+			<v-toolbar dark color="primary">
+				<v-toolbar-title>Registration</v-toolbar-title>
+			</v-toolbar>
 				<v-card-text>
-					<v-form v-model="valid" ref="form"
-					validation>
-					<v-text-field
-						prepend-icon="mdi-account" 							
-						name="email" 
-						label="Email" 
-						type="email"
-						v-model="email"
-						:rules="emailRules">
-					</v-text-field>
-					<v-text-field
-						prepend-icon="mdi-lock" 
-						name="password" 
-						label="Password" 
-						type="password"
-						v-model="password"
-						:rules="passwordRules">
-					</v-text-field>
-					</v-form> 
+					<v-form v-model="valid" ref="form" validation>
+						<v-text-field
+							prepend-icon="mdi-account" 
+							name="email" 
+							label="Email" 
+							type="email" 
+							v-model="email"
+							:rules="emailRules"
+							>
+						</v-text-field>
+						<v-text-field  
+							prepend-icon="mdi-lock" 
+							name="password" 
+							label="Password" 
+							type="password" 
+							v-model="password"
+							:rules="passwordRules" 
+							>
+						</v-text-field>
+						<v-text-field  
+							prepend-icon="mdi-lock" 
+							name="confirm-password" 
+							label="Confirm Password" 
+							type="password" 
+							v-model="confirmPassword"
+							:rules="confirmPasswordRules" 
+							>
+							</v-text-field>
+						</v-form> 
 				</v-card-text>
 				<v-card-actions>
 					<v-spacer></v-spacer>
 					<v-btn 
 					color="primary"
-					@ckick="OnSubmit"
+					@click="onSubmit"
 					:disabled="!valid"
 					>
 						Create Account
@@ -42,23 +54,28 @@
 </template>
 <script>
 export default{
-	data(){
+	data(){ 
 		return{
 			email: "",
 			password: "",
+			confirmPassword: "",
 			valid: false,
 			emailRules: [
-				v => !!v || 'E-mail is required',
-				v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+			v => !!v || 'E-mail is required',
+			v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
 			],
 			passwordRules: [
-				v => !!v || 'Name is required',
-				v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters',
+			v => !!v || 'Password is required',
+			v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters'
+			],
+			confirmPasswordRules: [
+			v => !!v || 'Password is required',
+			v => v === this.password || 'Password should match'
 			]
-		}
+		}	
 	},
 	methods: {
-		OnSubmit(){
+		onSubmit(){
 			if (this.$refs.form.validate()){
 				const user = {
 					email: this.email,
