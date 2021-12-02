f05f049aa80cdeeef85b1c36485e75af2e5bbdd8
nazartsap <tsap.n.i20@gmail.com>
2021-12-02 19:05:47 +0300

    9.2

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,9 +1,9 @@
 <template> 
 	<v-container>
-		<v-layout row> 
-			<v-flex xs12>
-<v-card class="mt-5">
-<v-img
+	<v-layout row> 
+	<v-flex xs12>
+	<v-card class="mt-5">
+	<v-img
 	height="300px"
 	src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
 	></v-img>
@@ -13,20 +13,20 @@
 	</v-card-text>
 	<v-card-actions>
 	<v-spacer></v-spacer>
-<v-btn text class="warning">Edit</v-btn>
+	<v-btn text class="warning">Edit</v-btn>
 	<v-btn class="success">Buy</v-btn>
 	</v-card-actions>	
-</v-card>
-
-	</v-flex>
-		</v-layout> 
-	</v-container>
+	</v-card>
+</v-flex>
+</v-layout> 
+</v-container>
 </template>
 <script>
 export default {
 	data () { 
-		return {
+return {
 		} 	
 	}
 } 
 </script>
+
