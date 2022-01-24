0d8d9d6aac421710fb3b46fc471c8eaaa2bd42c6
nazartsap <tsap.n.i20@gmail.com>
2022-01-24 20:43:09 +0300

    21.3

--- src/components/Shared/BuyModal.vue
+++ src/components/Shared/BuyModal.vue
@@ -1,76 +1,98 @@
-<template v-slot:activator="{ on }"><v-btn 
-          v-on="on"
-          color="primary"
+<template>
+	<v-dialog
+		v-model="modal"
+		width="500px"
+	>
+		<template v-slot:activator="{ on }">
+            <v-btn 
+                v-on="on"
+                dark raised color="#B85252"
             >
-        Buy</v-btn></template>
-          <v-container>
-              <v-layout row>
-                  <v-flex xs>
-                      <v-card-title>
-                     <h1 class="text--primary">Do you wan't to buy it?</h1>
+            Buy
+            </v-btn>
+        </template>
+		<v-card>
+			<v-container>
+				<v-layout row>
+					<v-flex xs12>
+						<v-card-title>
+                            <h1 class="text--primary">Do you wan't to buy it?</h1>
                         </v-card-title>
+					</v-flex>
+				</v-layout>
+				<v-layout row>
+					<v-flex xs>
+						<v-card-text>
+							<v-text-field
+								name="name" 
+                                label="Your name" 
+                                type="text" 
+                                v-model="name" 
+							>
+							</v-text-field>
+							<v-text-field
+                                name="phone" 
+                                label="Your phone" 
+                                type="text" 
+                                v-model="phone" 
+                            >
+                            </v-text-field>
+						</v-card-text>          			
+					</v-flex>
+				</v-layout>
+				<v-layout row>
+					<v-flex xs>
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn text @click="onCancel"
+                            :disabled="localLoading"
+                            >Close</v-btn>
+							<v-btn color="success" @click="onSave"
+                            :disabled="localLoading"
+                            :loading="localLoading"
+                            >Buy it!</v-btn>
+						</v-card-actions>         			
+					</v-flex>
+				</v-layout>
+			</v-container>
+		</v-card>
+	</v-dialog>
+</template>
 
-                  </v-flex>
-              </v-layout>
-              <v-layout row>
-                  <v-flex xs>
-                      <v-card-text>
-                        <v-text-field
-                        name="name" 
-                            label="Your name" 
-                            type="text" 
-                            v-model="name" 
-                        >
-                        </v-text-field>
-                    <v-text-field
-                    name="phone" 
-                    label="Your phone" 
-                    type="text" 
-                v-model="phone" 
-                v-model="phone" 
-                </v-text-field>      
-                    
-                  </v-flex>
-              </v-layout>
-              <v-layout row>
-                  <v-flex xs>
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn text @click="onCancel">Close</v-btn>
-                        <v-btn color="success" @click="onSave">Save</v-btn>
-                        </v-card-actions> 
-                    
-                  </v-flex>
-              </v-layout>
-          </v-container>
 <script>
-
     export default {
         props: ['ad'],
-        data() {
-            return {
-                modal: false,
-                editedTitle: this.ad.title,
-                editedDesc: this.ad.desc
-
+            data() {
+                return {
+                    modal: false,
+                    name: "",
+                    phone: "",
+                    localLoading: false
+                }
+            },
+            methods: {
+                onCancel (){
+                    this.name = ""
+                    this.phone = ""
+                    this.modal = false
+                },
+                onSave (){
+                    if (this.name !== '' && this.phone !== '') {
+                        this.localLoading = true    
+                        this.$store.dispatch('createOrder', {
+                            name: this.name,
+                            phone: this.phone,
+                            adId: this.ad.id,
+                            ownerId: this.ad.ownerId
+                        })
+                        .finally(() => {
+                            this.localLoading = false
+                            this.name = ""
+                            this.phone = ""
+                            this.modal = false
+                        })
+                    }
+                }
             }
-    },
-    methods: {
-    onCancel (){
-    this.editedTitle = this.ad.title
-    this.editedDesc = this.ad.desc
-    this.modal = false
-    },
-    onSave (){
-    if (this.editedTitle !== '' && this.editedDesc !== '') {
-    this.$store.dispatch('updateAd', {
-            title: this.editedTitle,
-            desc: this.editedDesc,
-            id: this.ad.id
-        })
-    this.modal = false
-            }
-        }
-    } 
-}
+        }	
 </script>
