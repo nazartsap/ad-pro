7a5a172124abd3408a071a722983af0da5d43276
nazartsap <tsap.n.i20@gmail.com>
2022-01-24 19:42:17 +0300

    20.3

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,59 +1,60 @@
 <template> 
-	<v-container>
-		<v-layout row> 
-			<v-flex xs12>
-				<v-card class="mt-5" v-if="!loading">
-							<v-img 
-							height="300px"
-							:src="ad.src">
-							</v-img>
-							<v-card-text>
-								<h1 class="text--primary mb-5"> {{ ad.title }} </h1>
-								<p> {{ ad.desc }} </p>
-							</v-card-text>
-							<v-card-actions>
-              <v-spacer></v-spacer>
-              <modal-dialog :ad="ad" v-if="isOwner"></modal-dialog>
-              <v-btn class="success">Buy</v-btn>
-            </v-card-actions>
-				</v-card>
-				<div v-else>
-					<v-container>
-						<v-layout row>
-							<v-flex xs12 sm2 offset-sm6 class="mt-5">
-								<v-progress-circular 
-								:size="70"
-								:width="7"
-								color="primary"
-								indeterminate
-								></v-progress-circular>
-							</v-flex>
-						</v-layout>
-					</v-container>
-				</div>
-			</v-flex>
-		</v-layout>
-	</v-container>
+  <v-container>
+    <v-layout row> 
+      <v-flex xs12>
+        <v-card class="mt-5" v-if="!loading">
+              <v-img 
+              height="500px"
+              :src="ad.src">
+              </v-img>
+              <v-card-text>
+                <h1 class="text--primary mb-5"> {{ ad.title }} </h1>
+                <p> {{ ad.desc }} </p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <modal-dialog :ad="ad" v-if="isOwner">
+                </modal-dialog>
+                <v-btn text class="success">Buy</v-btn>
+              </v-card-actions>
+        </v-card>
+        <div v-else>
+          <v-container>
+            <v-layout row>
+              <v-flex xs12 sm2 offset-sm6 class="mt-5">
+                <v-progress-circular 
+                :size="70"
+                :width="7"
+                color="primary"
+                indeterminate
+                ></v-progress-circular>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 import EditAdModal from './EditAdModal'
 export default {
-    props:['id'],
-    computed:{
-        ad(){
-            const id=this.id
-            return this.$store.getters.adById(id)
-        },
-		loading(){
-			return this.$store.getters.loading
-		},
-    isOwner () {
-	return this.ad.ownerId === this.$store.getters.user.id
-        }
+  props: ['id'],
+  computed: {
+    ad() {
+      const id = this.id
+      return this.$store.getters.adById(id)
     },
-	components: {
-		'modal-dialog': EditAdModal
-	}
+    loading () {
+      return this.$store.getters.loading
+    },
+    isOwner () {
+      return this.ad.ownerId === this.$store.getters.user.id
+    }
+  },
+  components: {
+    'modal-dialog': EditAdModal
+  }
 } 
 </script>
--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,47 +1,57 @@
 <template> 
-	<v-container>
-		<v-layout row> 
-			<v-flex xs12 sm6 offset-sm3>
-				<h1 class="text--secondary mb-3 mt-3">My ads</h1>
-				<v-card class="elevation-10 mb-5" 
-					v-for="ad in myAds" 
-					:key="ad.id">
-					<v-layout row>
-						<v-flex xs4 class="pl-3">
-							<v-img
-								:src="ad.src"
-								height="175px">
-							</v-img>	
-						</v-flex>
-						<v-flex xs8 class="pr-3">
-						<v-card-text>
-							<h2 class="text--primary">{{ad.title}}</h2>
-							<p>{{ad.desc}}</p>
-						</v-card-text>
-						<v-card-actions>
-							<v-spacer></v-spacer>
-							<v-btn 
-								color="primary"
-								:to="'/ad/' + ad.id">
-								Open
-							</v-btn>
-						</v-card-actions>	
-						</v-flex>
-					</v-layout>
-				</v-card>
-			</v-flex>
-		</v-layout>
-	</v-container>
+    <v-container>
+        <v-layout row v-if="!loading && myAds.length !== 0"> 
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-5 mt-5">My ads</h1>
+              <v-card 
+                  class="elevation-10 mb-5" v-for="ad in myAds" :key="ad.id">
+                  <v-layout row>
+                        <v-flex xs4 class="pl-3">
+                          <v-img :src="ad.src" height="175px"></v-img>	
+                        </v-flex>
+                       <v-flex xs8 class="pr-3">
+                          <v-card-text>
+                             <h2 class="text-primary mb-5">{{ad.title}}</h2>
+                             <p>{{ad.desc}}</p>
+                          </v-card-text>
+                            <v-card-actions>
+                              <v-spacer></v-spacer>
+                              <v-btn dark color="success" :to="'/ad/' + ad.id">
+                                 Open
+                              </v-btn>
+                           </v-card-actions>	
+                       </v-flex>
+                   </v-layout>
+              </v-card>
+            </v-flex>
+        </v-layout> 
+        <v-layout raw v-else-if="!loading && myAds.length === 0">
+          <v-flex xs12 sm6 offset-sm3>
+            <h1 class="text--primary">You have no ads.</h1>
+            </v-flex>			
+        </v-layout>
+            <v-layout raw v-else>
+              <v-flex xs12 sm2 offset-sm6 class="mt-5">
+                <v-progress-circular 
+                  :size="70"
+                  :width="7"
+                  color="primary"
+                  indeterminate
+                ></v-progress-circular>
+              </v-flex>
+              </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-	computed: {
-		myAds(state, getters) {
-	return state.ads.filter(ad => {
-			return ad.ownerId == getters.user.id
-	})       
+    computed: {
+        myAds(){
+            return this.$store.getters.myAds
+        },
+        loading () {
+            return this.$store.getters.loading
+        }
     }
-}
 } 
 </script>
