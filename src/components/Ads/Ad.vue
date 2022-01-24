89b367fa1cf6d9e7cd8940cd8cddd6e55a4fc368
nazartsap <tsap.n.i20@gmail.com>
2022-01-24 18:21:04 +0300

    19.2

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -1,35 +1,40 @@
-<template>
+<template> 
   <v-container>
-  <v-layout row>
-  <v-flex xs12>
-  <v-card class="mt-5">
-  <v-img
-  height="300px"
-  :src="ad.src"
-  ></v-img>
-  <v-card-text>
-  <h1 class="text--primary mb-3">{{ ad.title }}</h1>
-  <p>{{ ad.desc }}</p>
-  </v-card-text>
-  <v-card-actions>
-  <v-spacer></v-spacer>
-  <v-btn text class="warning">Edit</v-btn>
-  <v-btn class="success">Buy</v-btn>
-  </v-card-actions>
-  </v-card>
-</v-flex>
-</v-layout>
-</v-container>
+    <v-layout row> 
+      <v-flex xs12>
+        <v-card class="mt-5">
+              <v-img 
+              height="300px"
+              :src="ad.src">
+              </v-img>
+              <v-card-text>
+                <h1 class="text--primary mb-5"> {{ ad.title }} </h1>
+                <p> {{ ad.desc }} </p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <modal-dialog>
+                </modal-dialog>
+                <v-btn text class="success">Buy</v-btn>
+              </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import EditAdModal from './EditAdModal'
 export default {
-  props: ['id'],
-   computed: {
-    ad() {
-    const id = this.id
-    return this.$store.getters.adById(id)
-    }
+    props:['id'],
+    computed:{
+        ad(){
+            const id=this.id
+            return this.$store.getters.adById(id)
+        }
+    },
+  components: {
+    'modal-dialog': EditAdModal
   }
-}
+} 
 </script>
--- src/components/Ads/EditAdModal.vue
+++ src/components/Ads/EditAdModal.vue
@@ -4,47 +4,48 @@
         width="400px"
       >
        <template v-slot:activator="{ on }"><v-btn 
-      	v-on="on"
-      	color="warning"
-      	flat
-      	>
+          v-on="on"
+          color="warning"
+          flat
+          >
         Edit</v-btn></template>
-      	<v-card>
+          <v-card>
           <v-container>
-          	<v-layout row>
-          		<v-flex xs>
-          			<v-card-title>
-          				<h1 class="text--primary">Edit Ad</h1>
-          			</v-card-title>
-          		</v-flex>
-          	</v-layout>
-          	<v-layout row>
-          		<v-flex xs>
-          			<v-card-text>
-          				Lorem ipsum.
-          			</v-card-text>          			
-          		</v-flex>
-          	</v-layout>
-          	<v-layout row>
-          		<v-flex xs>
-          			<v-card-actions>
-          				<v-spacer></v-spacer>
-          				<v-btn color="">Cancel</v-btn>
-          				<v-btn color="success">Save</v-btn>
-          			</v-card-actions>         			
-          		</v-flex>
-          	</v-layout>
+              <v-layout row>
+                  <v-flex xs>
+                      <v-card-title>
+                          <h1 class="text--primary">Edit Ad</h1>
+                      </v-card-title>
+                  </v-flex>
+              </v-layout>
+              <v-layout row>
+                  <v-flex xs>
+                      <v-card-text>
+                          Lorem ipsum.
+                      </v-card-text>          			
+                  </v-flex>
+              </v-layout>
+              <v-layout row>
+                  <v-flex xs>
+                      <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn color="">Cancel</v-btn>
+                          <v-btn color="success">Save</v-btn>
+                      </v-card-actions>         			
+                  </v-flex>
+              </v-layout>
           </v-container>
         </v-card>
       </v-dialog>
 </template>
 <script>
-	export default {
-		data() {
-			return {
-				modal: false
-			}
-		}
-	}
-	
+
+    export default {
+        data() {
+            return {
+                modal: false
+            }
+        }
+    }
+    
 </script>
