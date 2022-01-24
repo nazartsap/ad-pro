31e721e0a06a577cdb996d616e997a98735b2128
nazartsap <tsap.n.i20@gmail.com>
2022-01-24 18:07:44 +0300

    18.3

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -4,69 +4,67 @@
             <v-flex xs12 sm6 offset-sm3>
                 <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
                 <v-form v-model="valid" ref="form" validation>
-                    <v-text-field
-                        name="title" 
-                        label="Ad Title" 
-                        type="text" 
-                        v-model="title"
-                        :rules="[ v => !!v || 'Title is required']" 
-                    >
-                    </v-text-field>
-                    <v-textarea  
-                        name="description" 
-                        label="Ad Description" 
-                        type="text" 
-                        v-model="description"
-                        :rules="[ v => !!v || 'Description is required']"
-                        class="mb-3" 
-                    ></v-textarea>
-                    </v-form>
-                    <v-layout  row>
-                        <v-flex xs12>
-                            <v-btn 
-                                class="mt-3"
-                                color="warning"
-                                @click="triggerUpload">
-                                Upload
-                                <v-icon right dark>mdi-cloud-upload</v-icon>
-                            </v-btn>
-                            <input 
-                                ref="fileInput" 
-                                type="file" 
-                                style="display:none;" 
-                                accept="image/*"
-                                @change="onFileChange"
-                            >
-                        </v-flex>
-                    </v-layout>
-                    <v-layout row>
-                        <v-flex xs12>
-                            <img :src="imageSrc" height="150" class="mt-3" v-if="imageSrc">
-                        </v-flex>
-                    </v-layout>
-                    <v-layout row>
-                        <v-flex xs12>
-                            <v-switch
-                            v-model="promo"
-                            label="Ad to Promo?"
-                            ></v-switch>
-                        </v-flex>
-                    </v-layout>
-                    <v-layout row>
-                        <v-flex xs12>
-                            <v-spacer></v-spacer>
-                            <v-btn 
-                                color="success"
-                                @click="createAd"
-                                :loading="loading"
-                                :disabled:="!valid || !image || loading" 
-                            >Create Ad</v-btn>
-                        </v-flex>
-                    </v-layout>
+                  <v-text-field color="#800080"
+                     name="title" 
+                     label="Ad Title" 
+                     type="text" 
+                     v-model="title"
+                     :rules="[ v => !!v || 'Title is required']">
+                 </v-text-field>
+                  <v-textarea color="#800080"  
+                     name="description" 
+                     label="Ad Description" 
+                     type="text" 
+                     v-model="description"
+                     :rules="[ v => !!v || 'Description is required']"
+                     class="mb-3" >
+                 </v-textarea>
+                </v-form>
+
+                <v-layout  row> 
+                  <v-flex xs12>
+                      <v-btn class="mt-3" color="warning" @click="triggerUpload">
+                          Upload
+                         <v-icon right dark>mdi-cloud-upload</v-icon>
+                      </v-btn>
+                      <input ref="fileInput"
+                      type = "file"
+                      style="display:none"
+                      accept="image/*"
+                      @change="onFileChange">
+                    </v-flex>
+                </v-layout>
+
+                <v-layout  row> 
+                 <v-flex xs12>
+                      <img :src="imageSrc" height="150" class="mt-3" v-if="imageSrc">
+                 </v-flex>
+                </v-layout>
+
+                <v-layout  row> 
+                  <v-flex xs12>
+                      <v-switch v-model="promo" color="#800080" label="Ad to Promo?"></v-switch>
+                    </v-flex>
+                </v-layout>
+
+                <v-layout  row> 
+                  <v-flex xs12>
+                      <v-spacer></v-spacer>
+                      <v-btn color="success" 
+                      @click="createAd"
+                      :loading= "loading"
+                      :disabled= "!valid || !image || loading"
+                      >Create Ad</v-btn>
+                  </v-flex>
+                </v-layout>
             </v-flex>
-        </v-layout>
+        </v-layout> 
     </v-container>
 </template>
+
+
+
+
 <script>
 export default {
     data () { 
@@ -76,43 +74,37 @@ export default {
             description: "",
             promo: false,
             image: null,
-            imageSrc: ""
+            imageSrs: ""
         } 	
     },
-    computed: {
-        loading() {
-            return this.$store.getters.loading
-        }
-        
-    },
     methods: {
-        triggerUpload () {
-            this.$refs.fileInput.click()
-        },  
-        createAd(){
-            if (this.$refs.form.validate() && this.image){
-            const ad = {
-                title: this.title,
-                desc: this.description,
-                promo: this.promo,
-                image: this.image
-            }
-            this.$store.dispatch("createAds", ad)
-            .then(() => {
-                this.$router.push("/list")
-            })
-            .catch(() => {})
-            }
-        },
-        onFileChange (event) {
-            const file = event.target.files[0]
-            const reader = new FileReader()
-            reader.onload = e => {
-                console.log(e)
-                this.imageSrc = reader.result
-            }
-            reader.readAsDataURL(file)
-            this.image = file
+      createAd(){
+          if (this.$refs.form.validate() && this.image){
+          const ad = {
+         title: this.title,
+         desc: this.description,
+         promo: this.promo,
+         image: this.image
+         }
+         this.$store.dispatch("createAd", ad)
+           .then(()=>{
+               this.$router.push("/list")
+           })
+           .catch(() =>{})
+         }
+       },
+       triggerUpload(){
+         this.$refs.fileInput.click()
+       },
+       onFileChange(event){
+           const file = event.target.files[0]
+           const reader = new FileReader()
+           reader.onload = e => {
+               console.log(e)
+               this.imageSrc = reader.result
+           }
+           reader.readAsDataURL(file)
+           this.image = file
         }
     }
 } 
--- src/store/ads.js
+++ src/store/ads.js
@@ -12,68 +12,95 @@ class Ad {
   }  
 
 export default {
-  state: {
-    ads:[/*
-                {
-                    title:"First",
-                    desc:"First Desc",
-                    promo: true,
-                    src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-                    id:"1"
-                },
-                {
-                    title:"Second",
-                    desc:"Second Desc",
-                    promo: true,
-                    src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-                    id:"2"
-                },
-                {
-                    title:"Third",
-                    desc:"Thitd Desc",
-                    promo: true,
-                    src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-                    id:"3"
-                },
-                {
-                    title:"Fouth",
-                    desc:"Fouth Desc",
-                    promo: true,
-                    src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-                    id:"4"
-                }
-              */]
+	state: {
+		ads:[
+			{
+				title:"Galaxy Tab A7",
+				desc:"Android 11 Qualcomm SM6115 Snapdragon 64GB 3GB",
+				promo: true,
+				src: "https://wallpapershome.ru/images/pages/pic_h/22770.jpg",
+				id:"1"
+			},
+			{
+				title:"Lenovo - IdeaPad",
+				desc:"IdeaPad Duet 5 Snapdragon SC7180 8GB RAM 128GB SSD",
+				promo: true,
+				src: "https://wallpapershome.ru/images/pages/pic_h/13301.jpg",
+				id:"2"
+			},
+			{
+				title:"Microsoft Surface",
+				desc:"Screen 14.4 IPS 120Hz, Multitouch, RAM 16GB SSD 512GB",
+				promo: true,
+				src: "https://wallpapershome.ru/images/pages/pic_h/23314.jpg",
+				id:"3"
+			},
+			{
+				title:"Samsung Galaxy Tab S7 WiFi",
+				desc:"SM-T220, 3GB, 32GB, Android 11",
+				promo: true,
+				src: "https://i1.wp.com/mynexttablet.com/wp-content/uploads/2021/09/samsung-galaxy-tab-s7-fe-tablet-comparison.jpg?fit=1920%2C1080&ssl=1",
+				id:"4"
+			},
+			{
+				title:"Xiaomi Pad 5",
+				desc:"Qualcomm Snapdragon 860 120 Hz",
+				promo: true,
+				src: "https://cdn.andro4all.com/files/2021/10/Diseno-de-la-Xiaomi-Pad-5.jpg",
+				id:"5"
+			},
+			{
+				title:"APPLE iPad mini",
+				desc:"Display Liquid Retina XDR4 12,9 Liquid Retina 11",
+				promo: true,
+				src: "https://www.iphones.ru/wp-content/uploads/2021/10/ipad-mini-6-review-iphonesru-10.jpg",
+				id:"6"
+			}
+    ]
     },
     mutations: {
         createAd(state, payload){
             state.ads.push(payload)
+        },
+        loadAds (state, payload) {
+            state.ads = payload
         }
+        
     },
     actions: {
-        createAd({commit},payload){
+        createAd({commit}, payload){
             payload.id = Math.random()
             commit('createAd', payload)
         },
         async createAds ({commit, getters}, payload) {
             commit('clearError')
             commit('setLoading', true)
+            const image = payload.image
       
             try {
               const newAd = new Ad(
                 payload.title,
                 payload.desc,
                 getters.user.id,
-                payload.src,
+                "",
                 payload.promo,
                 payload.id
             )
               const fbValue = await fb.database().ref('ads').push(newAd)
-              console.log ("bbbb")
-              console.log (fbValue.key)
+              const imageExt = image.name.slice(image.name.lastIndexOf("."))
+
+              await fb.storage().ref().child(`ads/${fbValue.key}.${imageExt}`).put(image).then(snapshot => {
+                snapshot.ref.getDownloadURL().then((downloadURL) => {
+                  const src = downloadURL
+                  fb.database().ref("ads").child(fbValue.key).update({ src })
+      
               commit('setLoading', false)
               commit('createAd', {
                 ...newAd,
-                id: fbValue.key
+                id: fbValue.key,
+                src
+                })
+              })
               })
             } catch (error) {
               commit('setError', error.message)
@@ -82,10 +109,11 @@ export default {
             }
         },
         async fetchAds({commit}) {
-          commit('clearError')
-              commit('setLoading', true)
-             try {
-            //Здесь запрос к базе данных
+            commit('clearError')
+            commit('setLoading', true)
+            try {
+                //Здесь запрос к базе данных
+                commit('setLoading', false)
                 const fbVal = await fb.database().ref('ads').once('value')
                 const ads = fbVal.val()
                 console.log(ads)
@@ -105,25 +133,27 @@ export default {
                     )
                   })
                   commit('loadAds', resultAds)
-             commit('setLoading', false)
-              }  catch (error) {
-              commit('setError', error.message)
-              commit('setLoading', false)
-              throw error
-             }
-          }
+
+                  
+                
+            }  catch (error) {
+                commit('setError', error.message)
+                commit('setLoading', false)
+                throw error
+            }
+        }
     },   
-  getters: {
-    ads(state) {
-      return state.ads
-    },
-    promoAds(state) {
-      return state.ads.filter(ad => {
-        return ad.promo
-      })
-    },
-    myAds(state) {
-      return state.ads
+	getters: {
+		ads(state) {
+			return state.ads
+		},
+		promoAds(state) {
+			return state.ads.filter(ad => {
+				return ad.promo
+			})
+		},
+		myAds(state) {
+			return state.ads
         },
         adById(state) {
             return id => {
